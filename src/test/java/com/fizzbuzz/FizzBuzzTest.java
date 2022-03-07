7874b9b525b3b0f380aa44e8ae60afda07f8998e
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-07 10:50:40 +0100

    Tests for fizzbuzz

--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,44 @@ package com.fizzbuzz;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
-    
-    @Test
-    public void test_fizzbuzz_example() {
-        assertEquals(2, 2);
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
     }
 
+    @Test
+    public void numDivBy3_returnFizz() {
+        // Given
+        String expected = "Fizz";
+        // When
+        String actual = fizzBuzz.check(3);
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numDivBy5_returnBuzz() {
+        // Given
+        String expected = "Buzz";
+        // When
+        String actual = fizzBuzz.check(5);
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numDivBy3and5_returnFizzBuzz() {
+        // Given
+        String expected = "FizzBuzz";
+        // When
+        String actual = fizzBuzz.check(15);
+        // Then
+        assertEquals(expected, actual);
+    }
 }
