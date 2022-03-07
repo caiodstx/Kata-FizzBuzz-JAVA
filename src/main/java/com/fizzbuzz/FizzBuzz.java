e47c5ccf7ef861916d919fc8996cd8e559bfd3ab
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-07 10:51:21 +0100

    fizzbuzz functions made

--- src/main/java/com/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,17 @@
 package com.fizzbuzz;
 
 public class FizzBuzz {
-    
+    public String check(int i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+        return "null";
+    }
+
 }
